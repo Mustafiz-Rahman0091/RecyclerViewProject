a684e0530b46ac5703e5f4e54b924ffc4fba16db
Mustafiz-Rahman0091 <mustafiz.nsu132@gmail.com>
2017-10-11 00:15:32 +0600

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/recyclerviewproject/ListItem.java
@@ -0,0 +1,24 @@
+package net.simplifiedcoding.recyclerviewproject;
+
+/**
+ * Created by Biplob on 10/10/2017.
+ */
+
+public class ListItem {
+
+    private String head;
+    private String desc;
+
+    public ListItem(String head, String desc) {
+        this.head = head;
+        this.desc = desc;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
